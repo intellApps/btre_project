c0afaaea49a3170ff6da28c8f272594ef27f7145
intellApps <intelligentappsofficial@gmail.com>
2021-09-22 12:36:59 -0600

    Removed email from settings

--- btre/settings.py
+++ btre/settings.py
@@ -148,12 +148,7 @@ MESSAGE_TAGS = {
     messages.ERROR: 'danger'
 }
 
-# Email
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = '587'
-EMAIL_HOST_USER = 'oldienavie1@gmail.com'
-EMAIL_HOST_PASSWORD = 'Olakemi@1'
-EMAIL_USE_TLS = True
+
 
 try:
     from .local_settings import *
